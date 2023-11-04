7a55fe348797db7c6a38f9a93c60ae769bd1a8ea
Your Name <you@example.com>
2023-11-04 18:34:56 +0100

    securite bd

--- apis/database/queries.js
+++ apis/database/queries.js
@@ -1,10 +1,13 @@
+// Ajoutez cette ligne au début de votre fichier
+require('dotenv').config();
+
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "garage",
-  password: "123",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
